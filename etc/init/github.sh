9fef24e754a23f0ab0560ef247bcb6337c896ccc
ssuzu <ssuzu@se-1.net>
2015-07-06 09:36:30 +0900

    Update

--- etc/init/github.sh
+++ etc/init/github.sh
@@ -6,8 +6,4 @@ set -eu
 . $DOTPATH/etc/lib/vital.sh
 . $DOTPATH/etc/lib/standard.sh
 
-# This script is
-# Mac OS X only!!
-is_osx || exit
-
 git remote set-url origin git@ssh.github.com:ssuzu-home/dotfiles.git
